c2d74a9beb3ab3e45ab10211a9a5e8d08c15e988
sasi-reddy-thota <thotasasikumarreddy@gmail.com>
2025-10-08 17:41:58 +0530

    day_2 practice

---
+++ practice_sql/day_2.sql
@@ -0,0 +1,64 @@
+/**
+problem statement: https://leetcode.ca/2024-01-10-2995-Viewers-Turned-Streamers/#2995-viewers-turned-streamers
+
+Write a solution to find the number of streaming sessions for users whose first session was as a viewer.
+
+Return the result table ordered by count of streaming sessions, user_id in descending order.
+
+
+
+Input: 
+Sessions table:
++---------+---------------------+---------------------+------------+--------------+
+| user_id | session_start       | session_end         | session_id | session_type | 
++---------+---------------------+---------------------+------------+--------------+
+| 101     | 2023-11-06 13:53:42 | 2023-11-06 14:05:42 | 375        | Viewer       |  
+| 101     | 2023-11-22 16:45:21 | 2023-11-22 20:39:21 | 594        | Streamer     |   
+| 102     | 2023-11-16 13:23:09 | 2023-11-16 16:10:09 | 777        | Streamer     | 
+| 102     | 2023-11-17 13:23:09 | 2023-11-17 16:10:09 | 778        | Streamer     | 
+| 101     | 2023-11-20 07:16:06 | 2023-11-20 08:33:06 | 315        | Streamer     | 
+| 104     | 2023-11-27 03:10:49 | 2023-11-27 03:30:49 | 797        | Viewer       | 
+| 103     | 2023-11-27 03:10:49 | 2023-11-27 03:30:49 | 798        | Streamer     |  
++---------+---------------------+---------------------+------------+--------------+
+Output: 
++---------+----------------+
+| user_id | sessions_count | 
++---------+----------------+
+| 101     | 2              | 
++---------+----------------+
+Explanation
+- user_id 101, initiated their initial session as a viewer on 2023-11-06 at 13:53:42, followed by two subsequent sessions as a Streamer, the count will be 2.
+- user_id 102, although there are two sessions, the initial session was as a Streamer, so this user will be excluded.
+- user_id 103 participated in only one session, which was as a Streamer, hence, it won't be considered.
+- User_id 104 commenced their first session as a viewer but didn't have any subsequent sessions, therefore, they won't be included in the final count. 
+Output table is ordered by sessions count and user_id in descending order. 
+
+
+-- Table Creation:
+CREATE TABLE Sessions (
+    user_id INT,
+    session_start DATETIME,
+    session_end DATETIME,
+    session_id INT,
+    session_type ENUM('Viewer', 'Streamer')
+);
+
+INSERT INTO Sessions (user_id, session_start, session_end, session_id, session_type) VALUES
+(101, '2023-11-06 13:53:42', '2023-11-06 14:05:42', 375, 'Viewer'),
+(101, '2023-11-22 16:45:21', '2023-11-22 20:39:21', 594, 'Streamer'),
+(102, '2023-11-16 13:23:09', '2023-11-16 16:10:09', 777, 'Streamer'),
+(102, '2023-11-17 13:23:09', '2023-11-17 16:10:09', 778, 'Streamer'),
+(101, '2023-11-20 07:16:06', '2023-11-20 08:33:06', 315, 'Streamer'),
+(104, '2023-11-27 03:10:49', '2023-11-27 03:30:49', 797, 'Viewer'),
+(103, '2023-11-27 03:10:49', '2023-11-27 03:30:49', 798, 'Streamer'); **/
+
+with first_session_cte as(
+    select
+        *
+        ,first_value(session_type) over(PARTITION BY user_id ORDER BY session_start) as first_session
+    from `Sessions`
+)
+SELECT user_id,count(first_session) as sessions_count FROM first_session_cte
+WHERE first_session='Viewer' and session_type='Streamer'
+GROUP BY user_id
+ORDER BY sessions_count DESC,user_id DESC
